9da7841d91eedbce49dedf5cff2ee68ff5c375b4
Vaughan Stedman <vaughan.stedman@gmail.com>
2022-11-13 16:28:12 +1100

    wip: working PoC game of life

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Cell } from './components/cell';
+import { Grid } from './components/grid';
+
+function App() {
+  return (
+    <>
+      <Grid />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/cell.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import styles from "./cell.module.css";
+
+export const Cell = () => {
+  const [isActive, setIsActive] = useState(false)
+  return (
+    <span
+      draggable={false}
+      onMouseEnter={(event) => {
+        event.buttons === 1 && setIsActive(!isActive)
+      }}
+      onMouseDown={() => setIsActive(!isActive)}
+      className={`${isActive ? styles["active"] : styles["inactive"]}
+                  ${styles.cell}`
+      }
+    />
+  )
+}
